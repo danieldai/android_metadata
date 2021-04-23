6e1c68b07f73d4a8cb4f38c0fd9f91419a5de7c6
Marvin M <39344769+M123-dev@users.noreply.github.com>
2021-04-23 19:02:04 +0200

    null-safety

--- lib/android_metadata.dart
+++ lib/android_metadata.dart
@@ -6,8 +6,8 @@ class AndroidMetadata {
   static const MethodChannel _channel =
   const MethodChannel('plugins.mianjiajia.com/android_metadata');
 
-  static Future<Map<String, String>> get metaDataAsMap async {
-    final Map<String, String> metaDataMap =
+  static Future<Map<String, String>?> get metaDataAsMap async {
+    final Map<String, String>? metaDataMap =
     await _channel.invokeMapMethod('getMetaDataAsMap');
     return metaDataMap;
   }
